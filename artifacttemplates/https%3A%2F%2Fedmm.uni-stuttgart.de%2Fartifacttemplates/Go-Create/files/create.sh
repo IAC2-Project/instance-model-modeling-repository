2730c88d25f79ad6ad8e6928d6a9e294b796a6cc
Michael Wurster <miwurster@googlemail.com>
2021-03-05 10:04:49 +0100

    Add RabbitMQ type

--- artifacttemplates/https%3A%2F%2Fedmm.uni-stuttgart.de%2Fartifacttemplates/Go-Create/files/create.sh
+++ artifacttemplates/https%3A%2F%2Fedmm.uni-stuttgart.de%2Fartifacttemplates/Go-Create/files/create.sh
@@ -3,13 +3,17 @@ apt -y update
 apt -y install wget curl git unzip
 # https://github.com/canha/golang-tools-install-script
 wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash -s -- --version 1.7.6
-source ~/.bashrc
+source /root/.bashrc
+export GOROOT=/root/.go
+export PATH=$GOROOT/bin:$PATH
+export GOPATH=/root/go
+export PATH=$GOPATH/bin:$PATH
 go get -v github.com/Masterminds/glide
 go get -u github.com/FiloSottile/gvt
 file_name=$(find . -type f -name '*.zip')
-mkdir -d ~/go/src/${WORKDIR}
-unzip $file_name -d ~/go/src/${WORKDIR}
-cd ~/go/src/${WORKDIR}
+mkdir -p /root/go/src/${WORKDIR}
+unzip $file_name -d /root/go/src/${WORKDIR}
+cd /root/go/src/${WORKDIR}
 glide install
 gvt restore
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o /app ${ENTRYPOINT}
--- artifacttemplates/https%3A%2F%2Fedmm.uni-stuttgart.de%2Fartifacttemplates/SpringBoot-Create/files/create.sh
+++ artifacttemplates/https%3A%2F%2Fedmm.uni-stuttgart.de%2Fartifacttemplates/SpringBoot-Create/files/create.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt -y update
-apt -y install openjdk-11-jre-headless
+# apt -y install openjdk-11-jre-headless
+apt -y install openjdk-8-jre-headless
 file_name=$(find . -type f -name '*.jar')
-cp $file_name ./app.jar
+mv $file_name ./app.jar
 exit 0

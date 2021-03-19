2691b0f2ff99b6440a8ad1e3f195434ec2683274
Michael Wurster <miwurster@googlemail.com>
2021-03-19 14:30:44 +0100

    Update create script

--- artifacttemplates/https%3A%2F%2Fedmm.uni-stuttgart.de%2Fartifacttemplates/MySQL_DBMS-Create/files/create.sh
+++ artifacttemplates/https%3A%2F%2Fedmm.uni-stuttgart.de%2Fartifacttemplates/MySQL_DBMS-Create/files/create.sh
@@ -3,11 +3,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt -y update
 apt -y install wget
 wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.14-1_all.deb
-DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.14-1_all.deb
+dpkg -i mysql-apt-config_0.8.14-1_all.deb
 echo "mysql-server mysql-server/root_password password ${ROOT_PASSWORD}" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password ${ROOT_PASSWORD}" | debconf-set-selections
 apt -y install mysql-server
-sed -i 's/.*bind-address.*=.*127\.0\.0\.1.*/bind-address = 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s/.*bind-address.*=.*127\.0\.0\.1.*/bind-address = 0.0.0.0/g" /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s/3306/${PORT:-3306}/g" /etc/mysql/mysql.conf.d/mysqld.cnf
 systemctl stop mysql
 systemctl daemon-reload
 systemctl enable mysql
